4720e513c404a0c8c78d9f1d02ca91f51815b381
Oyjord <oyjord@gmail.com>
2025-10-03 13:10:57 -0700

    Update scrape_reign.rb

--- scrape_reign.rb
+++ scrape_reign.rb
@@ -2,8 +2,8 @@ require 'ahl_scraper'
 require 'json'
 require 'date'  # ✅ Add this to parse dates
 
-team_id = 404  # Ontario Reign
-season_id = 90 # current Regular Season maybe but empty
+team_id = 403  # Ontario Reign
+season_id = 89 # current Regular Season maybe but empty
 
 players = AhlScraper::RosterPlayers.retrieve_all(team_id, season_id).map do |player|
   birth_year = begin
